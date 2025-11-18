e67e9434cd457712d54307532741951d78a4f165
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-18 23:23:18 +0100

    Added AuthController and UserMapper classes with basic implementation

--- src/main/java/com/transportsecure/controller/AuthController.java
+++ src/main/java/com/transportsecure/controller/AuthController.java
@@ -1,4 +1,19 @@
 package com.transportsecure.controller;
 
+import com.transportsecure.dto.CreateUserDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
 public class AuthController {
+    @PostMapping
+    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserDTO user) {
+
+        return null;
+    }
 }
--- src/main/java/com/transportsecure/mapper/UserMapper.java
+++ src/main/java/com/transportsecure/mapper/UserMapper.java
@@ -1,4 +1,13 @@
 package com.transportsecure.mapper;
 
-public class UserMapper {
+import com.transportsecure.dto.CreateUserDTO;
+import com.transportsecure.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    User toEntity(CreateUserDTO user);
+    CreateUserDTO toDTO(User user);
 }
