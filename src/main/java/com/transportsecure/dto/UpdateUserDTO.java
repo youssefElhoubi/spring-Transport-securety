58a1f8ca1465b9763cfdaf3bbf13d8e0e26c11e3
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-18 21:06:23 +0100

    made a validation dto for updating a user

---
+++ src/main/java/com/transportsecure/dto/UpdateUserDTO.java
@@ -0,0 +1,18 @@
+package com.transportsecure.dto;
+
+import com.transportsecure.enums.Specialty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UpdateUserDTO {
+    @NotBlank(message = "Id is required to complete this process")
+    private String id;
+    @Size(min = 6,message = "password must at leat 6 characters ")
+    private String oldPassword;
+    @Size(min = 6,message = "password must at leat 6 characters ")
+    private String newPassword;
+    @Size(min = 6,message = "password must at leat 6 characters ")
+    private String confirmPassword;
+    private boolean active;
+    private Specialty specialty;
+}
