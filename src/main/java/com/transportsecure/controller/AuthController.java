f34237dd7cdfb0664c175b8d01f2df02f7cd1e5c
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-19 10:54:27 +0100

    Added UserService and JwtService dependencies to AuthController, implemented user creation and JWT token generation

--- src/main/java/com/transportsecure/controller/AuthController.java
+++ src/main/java/com/transportsecure/controller/AuthController.java
@@ -1,19 +1,35 @@
 package com.transportsecure.controller;
 
 import com.transportsecure.dto.CreateUserDTO;
+import com.transportsecure.entity.User;
+import com.transportsecure.service.JwtService;
+import com.transportsecure.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
-    @PostMapping
-    public ResponseEntity<?> signUp(@Valid @RequestBody CreateUserDTO user) {
+    private UserService userService;
+    private JwtService jwtService;
 
-        return null;
+    public AuthController(UserService userService, JwtService jwtService) {
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping
+    @PreAuthorize("@validationService.isValidSpecialty(#dto)")
+    public ResponseEntity<?> signUp(@Valid @RequestBody CreateUserDTO dto) {
+        User user = userService.createUser(dto);
+        String Token = jwtService.createToken(user.getId(), Map.of("role", user.getRole()));
+        return ResponseEntity.ok(Token);
     }
 }
