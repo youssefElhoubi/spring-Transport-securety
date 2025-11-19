2f7606d64878f0fc2b80c3e9aa57cca9ce3718b5
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-19 11:11:45 +0100

    Implement login functionality and user retrieval by email
    - Added LogInDto for login request validation
    - Introduced logIn method in AuthController for user authentication
    - Enhanced UserService with findByEmail method

--- src/main/java/com/transportsecure/controller/AuthController.java
+++ src/main/java/com/transportsecure/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.transportsecure.controller;
 
 import com.transportsecure.dto.CreateUserDTO;
+import com.transportsecure.dto.LogInDto;
 import com.transportsecure.entity.User;
 import com.transportsecure.service.JwtService;
 import com.transportsecure.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +21,25 @@ import java.util.Map;
 public class AuthController {
     private UserService userService;
     private JwtService jwtService;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthController(UserService userService, JwtService jwtService) {
+    public AuthController(UserService userService, JwtService jwtService , PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping
+    @PostMapping("signup")
     @PreAuthorize("@validationService.isValidSpecialty(#dto)")
     public ResponseEntity<?> signUp(@Valid @RequestBody CreateUserDTO dto) {
         User user = userService.createUser(dto);
+        String Token = jwtService.createToken(user.getId(), Map.of("role", user.getRole()));
+        return ResponseEntity.ok(Token);
+    }
+    @PostMapping("login")
+    public ResponseEntity<?> logIn(@Valid @RequestBody LogInDto dto){
+        User user = userService.findByEmail(dto.getEmail());
+        passwordEncoder.matches(dto.getPassword(), user.getPassword());
         String Token = jwtService.createToken(user.getId(), Map.of("role", user.getRole()));
         return ResponseEntity.ok(Token);
     }
--- src/main/java/com/transportsecure/service/UserService.java
+++ src/main/java/com/transportsecure/service/UserService.java
@@ -55,4 +55,8 @@ public class UserService {
             return null;
         });
     }
+
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
 }
