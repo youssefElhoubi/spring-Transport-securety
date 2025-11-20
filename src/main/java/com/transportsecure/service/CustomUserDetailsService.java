a175e12db22c41dff466c4318ef27556dcb43721
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-20 17:25:00 +0100

    Add JwtAuthenticationFilter and update CustomUserDetailsService with @Service annotation

--- src/main/java/com/transportsecure/service/CustomUserDetailsService.java
+++ src/main/java/com/transportsecure/service/CustomUserDetailsService.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
