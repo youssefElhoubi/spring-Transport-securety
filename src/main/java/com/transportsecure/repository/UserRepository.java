bef786a6954ba927d1cdcf876d984a3710ae9801
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-20 23:46:00 +0100

    working on the transporter controller

--- src/main/java/com/transportsecure/repository/UserRepository.java
+++ src/main/java/com/transportsecure/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.transportsecure.repository;
 
 import com.transportsecure.entity.User;
 import com.transportsecure.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,5 @@ import java.util.List;
 public interface UserRepository extends MongoRepository<User, String> {
     public User findByEmail(String email);
 
-    List<User> findAllByRole(Role role);
+    Page<User> findAllByRole(Role role, Pageable pageable);
 }
